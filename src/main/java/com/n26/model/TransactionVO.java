5b7e4adccedc3c20d5c37d2165669f76c09c9de8
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2021-05-02 13:00:48 +0530

    Adding field level validation in request vo

--- src/main/java/com/n26/model/TransactionVO.java
+++ src/main/java/com/n26/model/TransactionVO.java
@@ -2,11 +2,16 @@ package com.n26.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 public class TransactionVO {
+
+    @NotNull
     private String amount;
+
+    @NotNull
     private String timestamp;
     public BigDecimal getDecimalAmount() {
         return new BigDecimal(this.amount);
