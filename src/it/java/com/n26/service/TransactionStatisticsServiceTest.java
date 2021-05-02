89d40fda572b53d4b2361d1c880171f7f15dd7d6
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2021-05-02 14:50:50 +0530

    Adding transaction service with add transactions call + tests

---
+++ src/it/java/com/n26/service/TransactionStatisticsServiceTest.java
@@ -0,0 +1,51 @@
+package com.n26.service;
+
+import com.n26.model.Statistics;
+import com.n26.model.TransactionVO;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.LongStream;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TransactionStatisticsServiceTest extends TestCase {
+
+    @InjectMocks
+    TransactionStatisticsService transactionStatisticsService;
+    @Test
+    public void shouldAddTransactionToEntireMinute_fromGivenTransactionTime() {
+        Map<Long, Statistics> mockMap = Mockito.mock(Map.class);
+        Mockito.when(mockMap.getOrDefault(Mockito.anyLong(), Mockito.any(Statistics.class))).thenReturn(Mockito.mock(Statistics.class));
+        ReflectionTestUtils.setField(transactionStatisticsService, "statisticsConcurrentHashMap", mockMap);
+        Long now = System.currentTimeMillis() - 10000; // Now-10s
+        String s = Instant.ofEpochMilli(now).toString();
+        TransactionVO transactionVO = new TransactionVO();
+        transactionVO.setTimestamp(s);
+        transactionVO.setAmount("10.12");
+        transactionStatisticsService.addTransaction(transactionVO);
+        Mockito.verify(mockMap, Mockito.times(60)).putIfAbsent(Mockito.anyLong(), Mockito.any(Statistics.class));
+    }
+
+    @Test
+    public void shouldAddTransactionToEntireMinute_fromGivenTransactionTime_Functional() {
+        Map<Long, Statistics> mockMap = new HashMap<>();
+        ReflectionTestUtils.setField(transactionStatisticsService, "statisticsConcurrentHashMap", mockMap);
+        Long now = System.currentTimeMillis() - 10000; // Now-10s
+        Instant currentInstant = Instant.ofEpochMilli(now);
+        String s = currentInstant.toString();
+        TransactionVO transactionVO = new TransactionVO();
+        transactionVO.setTimestamp(s);
+        transactionVO.setAmount("10.12");
+        transactionStatisticsService.addTransaction(transactionVO);
+        LongStream.range(currentInstant.getEpochSecond(), currentInstant.getEpochSecond()+60).allMatch(mockMap::containsKey);
+        // Mockito.verify(mockMap, Mockito.times(60)).putIfAbsent(Mockito.anyLong(), Mockito.any(Statistics.class));
+    }
+}
--- src/main/java/com/n26/service/TransactionStatisticsService.java
+++ src/main/java/com/n26/service/TransactionStatisticsService.java
@@ -1,8 +1,10 @@
 package com.n26.service;
 
 import com.n26.model.Statistics;
+import com.n26.model.TransactionVO;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReadWriteLock;
@@ -18,4 +20,17 @@ public class TransactionStatisticsService {
     // Overall worst case complexity in Big-O ==> O(C) ==> O(1)
     // where C=119 is a constant and doesn't change with count of transactions
 
+    public void addTransaction(TransactionVO transaction) {
+        readWriteLock.writeLock().lock();
+        Instant timestamp = Instant.parse(transaction.getTimestamp());
+        long timestampEpochSecond = timestamp.getEpochSecond();
+        for(long ts = timestampEpochSecond; ts <timestampEpochSecond+60; ts++ ) {
+            Statistics s = statisticsConcurrentHashMap.getOrDefault(ts, new Statistics());
+            s.maxOf(transaction.getDecimalAmount());
+            s.minOf(transaction.getDecimalAmount());
+            s.add(transaction.getDecimalAmount());
+            statisticsConcurrentHashMap.putIfAbsent(ts, s);
+        }
+        readWriteLock.writeLock().unlock();
+    }
 }
